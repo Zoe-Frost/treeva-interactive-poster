5ba8f8832063e50f0921a2661564ee781fe8bfd1
Zoe-Frost <zoe.frost@mds.ac.nz>
2025-06-16 21:49:50 +1200

    js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 
+document.querySelector(".c")
+
+
 
 
 
@@ -6,8 +9,71 @@
 gsap.to(
     ".c",
     {
-        y:500,
-        duration: 2
+        y:82,
+        x:-160,
+        duration: 1,
+        ease:"bounce.out"
 
     }
 );
+
+gsap.to(
+    ".l",
+    {
+        y:170,
+        x:-285,
+        duration:1,
+        ease:"bounce.out"
+    }
+);
+
+gsap.to(
+    ".i",
+    {
+        y:260,
+        x:70,
+        duration:1,
+        ease:"bounce.out"
+    }
+);
+
+gsap.to(
+    ".m",
+    {
+        y:370,
+        x:-75,
+        duration:1,
+        ease:"bounce.out"
+    }
+);
+
+gsap.to(
+    ".b",
+    {
+        y:528,
+        x:-10,
+        duration:1,
+        ease:"bounce.out"
+    }
+);
+
+gsap.to(
+    ".e",
+    {
+        y:424,
+        x:356,
+        duration:1,
+        ease:"bounce.out"
+    }
+);
+
+gsap.to(
+    ".r",
+    {
+        y:620,
+        x:426,
+        duration:1,
+        ease:"bounce.out"
+    }
+);
+
