1a801547c2bf75a003a31103f3f0abb2912708e0
Zoe-Frost <zoe.frost@mds.ac.nz>
2025-06-17 15:26:48 +1200

    final

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const pauseButton = document.querySelector("#pause");
 const restartButton = document.querySelector("#reset");
 const animation = gsap.timeline({ paused: true });
 
-let isPaused = true(); 
+let isPaused = true; 
 
 
 playButton.addEventListener("click", () => {
@@ -28,76 +28,69 @@ restartButton.addEventListener("click", () => {
 
 
 
+animation
+    .to(
+        ".c",
+        {
+            y: 82,
+            x: -160,
+            duration: 1,
+            ease: "bounce.out"
 
-gsap.to(
-    ".c",
-    {
-        y:82,
-        x:-160,
-        duration: 2,
-        ease:"bounce.out"
-
-    }
-);
-
-gsap.to(
-    ".l",
-    {
-        y:170,
-        x:-285,
-        duration:2,
-        ease:"bounce.out"
-    }
-);
-
-gsap.to(
-    ".i",
-    {
-        y:260,
-        x:70,
-        duration:2,
-        ease:"bounce.out"
-    }
-);
-
-gsap.to(
-    ".m",
-    {
-        y:370,
-        x:-75,
-        duration:2,
-        ease:"bounce.out"
-    }
-);
-
-gsap.to(
-    ".b",
-    {
-        y:528,
-        x:-10,
-        duration:2,
-        ease:"bounce.out"
-    }
-);
-
-gsap.to(
-    ".e",
-    {
-        y:424,
-        x:356,
-        duration:2,
-        ease:"bounce.out"
-    }
-);
-
-gsap.to(
-    ".r",
-    {
-        y:620,
-        x:426,
-        duration:2,
-        delay:1,
-        ease:"bounce.out"
-    }
-);
+        }
+    )
+    .to(
+        ".l",
+        {
+            y: 170,
+            x: -285,
+            duration: 1,
+            ease: "bounce.out"
+        }
+    )
+    .to(
+        ".i",
+        {
+            y:260,
+            x:70,
+            duration:1,
+            ease:"bounce.out"
+        }
+    )
+    .to(
+        ".m",
+        {
+           y:370,
+           x:-75,
+           duration:1,
+           ease:"bounce.out"
+        }
+    )
+    .to(
+        ".b",
+        {
+           y:528,
+           x:-10,
+           duration:1,
+           ease:"bounce.out"
+        }
+    )
+    .to(
+        ".e",
+        {
+           y:424,
+           x:356,
+           duration:1,
+           ease:"bounce.out"
+        }
+    )
+    .to(
+        ".r",
+        {
+           y:620,
+           x:426,
+           duration:1,
+           ease:"bounce.out"
+        }
+    )
 
